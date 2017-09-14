06aedb70b9cf56614d8a1626ab2bf12d43ac63f7
Tivona Spruill <tivona.spruill@yahoo.com>
2017-09-13 22:18:02 -0400

    changed .map code

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -3,15 +3,15 @@ import moment from 'moment';
 import earthquakes from '../data/earthquakes.js';
 
 export default class EarthquakeList extends Component {
-  render(){
+  render() {
     let ListOfEarthquakes = earthquakes.features.map((earthquakes) => {
         return (
-          <div className="col-sm-6" {earthquakes.features.id}>
+          <div className="col-sm-6" key={earthquakes.id}>
             <div className="card">
               <div className="card-block">
-                <h4 className="card-title">{earthquakes.features.place}</h4>
-                <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquakes.features.mag}</h6>
-                <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquakes.features.time).format('llll')}</h6>
+                <h4 className="card-title">{earthquakes.properties.place}</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquakes.properties.mag}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquakes.properties.time).format('llll')}</h6>
                 <p className="card-text">Coordinates: {earthquakes.geometry.coordinates}</p>
 
                 <a href={"https://earthquake.usgs.gov/earthquakes/eventpage/ci37702767"} className="card-link">USGS Event Link</a>
