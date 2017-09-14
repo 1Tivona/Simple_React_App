38306660f094ca5e198b350cb098659f505a8e26
Tivona Spruill <tivona.spruill@yahoo.com>
2017-09-13 22:23:51 -0400

    corrected layout of divs

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import styles from '../styles/App.css';
-import EarthquakeList from '../components/EarthquakeList.js'
 import EarthquakeInfo from '../components/EarthquakeInfo.js'
+import EarthquakeList from '../components/EarthquakeList.js'
+
 
 class App extends Component {
   render() {
@@ -9,8 +10,9 @@ class App extends Component {
       <div className="App">
         <div className="title">
         <div className="my-header">"Earthquakes!"</div>
-        <EarthquakeList />
         <EarthquakeInfo />
+        <EarthquakeList />
+
         </div>
       </div>
     );
